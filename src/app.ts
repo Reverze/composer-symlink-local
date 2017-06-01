8b681c453c5104c81233e09eb434a1b3290ab6ab
Reverze <hawkmedia24@gmail.com>
2017-06-01 15:49:17 +0200

    Initial version

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,49 @@
 "use strict";
+import * as path from 'path';
+import * as fs from 'fs';
+import * as validator from 'validator';
+import InputParser from './input/parser';
+
+export default class Application
+{
+    /**
+     * Current application working directory
+     * @type {string}
+     */
+    private currentWorkingDirectory : string = null;
+
+    public constructor(workingDirectoryPath : string)
+    {
+        this.validateWorkingDirectoryPath(workingDirectoryPath);
+        this.currentWorkingDirectory = workingDirectoryPath;
+    }
+
+    /**
+     * Validates working directory path
+     * @param workingDirectoryPath
+     */
+    public validateWorkingDirectoryPath(workingDirectoryPath : string) : void
+    {
+        if (workingDirectoryPath === null || workingDirectoryPath === undefined)
+        {
+            throw new Error("Working directory path is not defined!");
+        }
+
+        if (validator.isEmpty(workingDirectoryPath)){
+            throw new Error("Working directory path was given but is empty!");
+        }
+
+        if (!fs.existsSync(workingDirectoryPath)){
+            throw new Error("Working directory path is not valid. Directory not exists in your filesystem");
+        }
+    }
+
+    public execute(args : Array<string>) : void
+    {
+        let parser : InputParser = new InputParser(args);
+
+    }
+
+
+}
 
