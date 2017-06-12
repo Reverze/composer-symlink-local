2948dd2d17d716b3cf29b26a1e0f6b8c491458b3
Reverze <hawkmedia24@gmail.com>
2017-06-12 19:19:35 +0200

    Update

---
+++ src/test/converter/MapModelConverter.spec.ts
@@ -0,0 +1,28 @@
+"use strict";
+import MapModelConverter from './../../converter/MapModelConverter';
+import MapModel from './../../model/MapModel';
+import MapNodeModel from './../../model/MapNodeModel';
+import { expect } from 'chai';
+
+describe("MapModelConverter Test", () => {
+    it("should convert raw object into model without errors", () => {
+        let rawNodes = [
+            {
+                source: "/home/my/Project",
+                linkName: "my-dependency"
+            },
+            {
+                source: "/home/my/Project2",
+                linkName: "my-dependency2"
+            }
+        ];
+
+        let map : MapModel = MapModelConverter.convertToMap(rawNodes);
+
+        for(let node of map.Nodes.each()){
+            console.log(node);
+        }
+
+        expect(map.Nodes.Count).to.equal(2);
+    });
+});
