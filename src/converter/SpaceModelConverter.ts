3d0faa193f129b0d61553a89f681c92fc434cd91
Reverze <hawkmedia24@gmail.com>
2017-06-12 19:40:42 +0200

    Initial version

---
+++ src/converter/SpaceModelConverter.ts
@@ -0,0 +1,69 @@
+/**
+ * This file is a part of ComposerSymlinkLocal
+ * @Author Reverze <hawkmedia24@gmail.com>
+ */
+"use strict";
+import ModelCollection from './../model/collection/ModelCollection';
+import SpaceModel from './../model/SpaceModel';
+import MapModel from './../model/MapModel';
+import MapModelConverter from './MapModelConverter';
+import Application from './../app';
+import * as path from 'path';
+
+class SpaceModelConverter
+{
+    public constructor()
+    {
+
+    }
+
+    /**
+     * Converts to model collection
+     * @param spaces
+     * @returns {ModelCollection<SpaceModel>}
+     */
+    public static convertToModelCollection(spaces : Array<any>) : ModelCollection<SpaceModel>
+    {
+        let spaceCollection : ModelCollection<SpaceModel> = new ModelCollection<SpaceModel>();
+
+        for(let rawSpace of spaces){
+            let space : SpaceModel = new SpaceModel();
+
+            SpaceModelConverter.validateRawSpaceObject(rawSpace);
+            space.Name = rawSpace.name;
+            space.DirectoryPath = path.resolve(Application.WorkingDirectory, rawSpace.rootDir);
+
+            let map : MapModel = MapModelConverter.convertToMap(rawSpace.map);
+
+            space.Map = map;
+            spaceCollection.push(space);
+        }
+
+        return spaceCollection;
+    }
+
+    /**
+     * Validates raw space object
+     * @param rawSpace
+     */
+    private static validateRawSpaceObject(rawSpace : any) : void
+    {
+        if (!rawSpace.name){
+            throw new Error("Space's name is not defined!");
+        }
+
+        if (!rawSpace.rootDir){
+            throw new Error("Space's root directory path is not defined!");
+        }
+
+        if (!rawSpace.map){
+            throw new Error("Space's map is not defined!");
+        }
+
+        if (!Array.isArray(rawSpace.map)){
+            throw new Error("Space's map is not an array!");
+        }
+    }
+}
+
+export default SpaceModelConverter;
