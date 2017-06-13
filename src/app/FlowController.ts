516c20dbe7a5222e569bfb7e5bf65ba44d08e401
Reverze <hawkmedia24@gmail.com>
2017-06-13 18:01:20 +0200

    Helps to manage flow control in app

---
+++ src/app/FlowController.ts
@@ -0,0 +1,17 @@
+"use strict";
+import * as process from 'process';
+
+class FlowController
+{
+    public constructor()
+    {
+
+    }
+
+    public exit()
+    {
+        process.exit();
+    }
+}
+
+export default FlowController;
