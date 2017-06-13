ed61a05107e49a2942f6cc676eb2c0df0f861ca2
Reverze <hawkmedia24@gmail.com>
2017-06-13 20:09:16 +0200

    Update

--- src/executor/CustomPwdExecutor.ts
+++ src/executor/CustomPwdExecutor.ts
@@ -2,7 +2,13 @@
 import Executor from './Executor';
 import FlowController from './../app/FlowController';
 import CustomPwdEventArgs from './../event/core/args/CustomPwdEventArgs';
+import * as fs from 'fs';
+import * as validator from 'validator';
 
+/**
+ * This executor is responsible to validate and save custom working directory's path
+ * provided by user.
+ */
 class CustomPwdExecutor extends Executor
 {
     public constructor()
@@ -12,9 +18,25 @@ class CustomPwdExecutor extends Executor
 
     public work(sender : FlowController, args : CustomPwdEventArgs)
     {
-        sender.Output.warning("siemka");
-        console.log(args);
-        console.log("should be executed here");
+        let customcwd : string|null = args.CustomWorkingDirectoryPath;
+
+        if (customcwd === null || customcwd === undefined){
+            sender.Output.error("Custom working directory path was not specified but option '--pwd' was passed!");
+            sender.exit();
+        }
+
+        if (validator.isEmpty(customcwd)){
+            sender.Output.error("Given custom working directory path is empty!");
+            sender.exit();
+        }
+
+        if (!fs.existsSync(customcwd)){
+            sender.Output.error("Directory is not exists on given custom working directory's path!");
+            sender.exit();
+        }
+
+        sender.WorkingDirectory = customcwd;
+
     }
 }
 
