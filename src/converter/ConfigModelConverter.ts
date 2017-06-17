b16901f2300318d59b3937ad1edcf3d544457537
Reverze <hawkmedia24@gmail.com>
2017-06-17 17:56:07 +0200

    Update

---
+++ src/converter/ConfigModelConverter.ts
@@ -0,0 +1,27 @@
+/**
+ * This file is a part of ComposerSymlinkLocal
+ * @Author Reverze <hawkmedia24@gmail.com>
+ */
+"use strict";
+import ModelCollection from './../model/collection/ModelCollection';
+import CommandModel from './../model/CommandModel';
+import SpaceModel from './../model/SpaceModel';
+import ConfigModel from './../model/ConfigModel';
+import CommandModelConverter from './CommandModelConverter';
+import SpaceModelConverter from './SpaceModelConverter';
+
+class ConfigModelConverter
+{
+    public constructor()
+    {
+
+    }
+
+    public convert(rawObject : any)
+    {
+
+    }
+
+}
+
+export default ConfigModelConverter;
