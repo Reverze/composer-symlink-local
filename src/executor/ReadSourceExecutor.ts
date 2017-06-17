8e2e25186a568a09fc5c5ab875a77be85af8533f
Reverze <hawkmedia24@gmail.com>
2017-06-17 20:12:17 +0200

    Update

--- src/executor/ReadSourceExecutor.ts
+++ src/executor/ReadSourceExecutor.ts
@@ -7,6 +7,8 @@ import Executor from './Executor';
 import FlowController from './../app/FlowController';
 import ConfigReader from './../config/ConfigReader';
 import EventArgs from './../event/EventArgs';
+import ConfigModel from './../model/ConfigModel';
+import ConfigModelConverter from './../converter/ConfigModelConverter';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as validator from 'validator';
@@ -21,9 +23,28 @@ class ReadSourceExecutor extends Executor
 
     public work(sender : FlowController, args : EventArgs)
     {
+        /**
+         * Initializes config file reader at given (or auto) working directory
+         * @type {ConfigReader}
+         */
         let configReader : ConfigReader = new ConfigReader(sender.WorkingDirectory, sender.SourceFileName);
 
-        let configObject = configReader.read();
+        /**
+         * Object from parsed config file
+         * @type {any}
+         */
+        let configObject : any = configReader.read();
+
+        let configModelInstance : ConfigModel = null;
+
+        try{
+            configModelInstance = ConfigModelConverter.convert(configObject);
+        }
+        catch(error){
+            sender.Output.error("Read config file failed!");
+            sender.Output.error(error.message);
+        }
+
 
 
     }
