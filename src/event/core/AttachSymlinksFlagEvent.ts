2d0bfd83642bf2ba49fea53baeaaa0aeb9a98407
Reverze <hawkmedia24@gmail.com>
2017-06-20 18:51:44 +0200

    Update

---
+++ src/event/core/AttachSymlinksFlagEvent.ts
@@ -0,0 +1,13 @@
+/**
+ * This file is a part of ComposerSymlinkLocal
+ * @Author Reverze <hawkmedia24@gmail.com>
+ */
+"use strict";
+import FlagEvent from './FlagEvent';
+
+class AttachSymlinksFlagEvent extends FlagEvent
+{
+
+}
+
+export default AttachSymlinksFlagEvent;
